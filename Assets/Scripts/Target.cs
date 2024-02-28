b1d1ca6cc2775e810f9e6ceae6b94db88dd5b140
Fabián E. García <fabianegl79@gmail.com>
2024-02-28 19:57:31 -0300

    Courrutina para crear objetos

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private float minSpeed = 12;
-    private float maxSpeed = 16;
+    private float minSpeed = 13;
+    private float maxSpeed = 18;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPosition = -6;
